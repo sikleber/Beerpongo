27f51e4b7724296a5cee91c700949130adcc18e7
Simon Kleber <kleber.simon@t-online.de>
2022-12-04 19:34:58 +0100

    Remove react frontend

--- sys-src/frontend/beerpongo-react/src/constants/BackendUrl.js
+++
@@ -1,4 +0,0 @@
-export const GET_GAME_URL = process.env.REACT_APP_BEERPONGO_BACKEND_URL + "/game/{GAME_ID}"
-export const PUT_GAME_URL = process.env.REACT_APP_BEERPONGO_BACKEND_URL + "/game"
-export const POST_GAME_URL = process.env.REACT_APP_BEERPONGO_BACKEND_URL + "/game"
-export const JOIN_GAME_URL = process.env.REACT_APP_BEERPONGO_BACKEND_URL + "/join/{GAME_ID}"
--- sys-src/frontend/beerpongo-react/src/context/UserContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-export const UserContext = createContext({
-    userID: -1,
-    setUserID: (prevState) => {},
-    gameID: -1,
-    setGameID: (prevState) => {}
-});
