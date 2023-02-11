8b03a5e4794b3ab630be73ca9cee043b566c4cf5
Simon Kleber <kleber.simon@t-online.de>
2023-02-11 09:24:47 +0100

    Remove unused zip script

--- sys-src/backend/scripts/zip_backend_layer.py
+++
@@ -1,7 +0,0 @@
-import shutil
-from os.path import dirname, realpath
-
-dir_path = dirname(dirname(realpath(__file__)))
-zip_source_dir = dir_path + "/requirements"
-zip_target_file = dir_path + "/lambda_layers/backend_lambda_layer"
-shutil.make_archive(zip_target_file, "zip", zip_source_dir)
